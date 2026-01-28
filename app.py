609be49c46a5684486b03815d3ebde49617b6627
gwardud3 <gdswimming@gmail.com>
2026-01-28 09:28:14 -0700

    Import placeholder_app in app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 from features.zone_map import zone_map_app
 from features.warehouse_map import warehouse_map_app
+from features.placeholder import placeholder_app
 
 st.set_page_config(
     page_title="Pricing Map Tools",
