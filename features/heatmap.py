950f6c10948698ff0b3d3f5d1bc54035fca9d0bd
gwardud3 <gdswimming@gmail.com>
2026-04-20 11:55:56 -0600

    Add display of DAS and EDAS ZIP counts

--- features/heatmap.py
+++ features/heatmap.py
@@ -136,6 +136,8 @@ def load_heatmap_data():
                 top_dim = dim_counts.index[0]
 
         das_zips, edas_zips = load_das_edas()
+        st.write(f"DAS ZIP count: {len(das_zips):,}")
+        st.write(f"EDAS ZIP count: {len(edas_zips):,}")
 
         # Make sure destzip is clean
         data["destzip"] = data["destzip"].astype(str).str.zfill(5)
