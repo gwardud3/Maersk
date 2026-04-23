0d3907bf38a98c1821a5b2841a21dc844145d456
gwardud3 <gdswimming@gmail.com>
2026-04-23 09:51:27 -0600

    Remove empty subheader from heatmap
    Remove empty subheader in heatmap display.

--- features/heatmap.py
+++ features/heatmap.py
@@ -234,7 +234,6 @@ def load_heatmap_data():
                 st.write("No ZIP classification available")
 
         with row1_col3:
-            st.subheader(" ")
             st.subheader("Key Details")
             
             avg_weight_display = (
